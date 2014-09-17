cf89de8aef0ef7ea06cdb81f5e66a243859faddc
Eric Steen <ericsteen1@gmail.com>
2014-09-16 20:38:46 -0700

    Inline prime calculation

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,23 +1,33 @@
+require 'inline'
+
 class Prime
   def self.first limit=1
     limit = Integer(limit)
     Prime.gather(limit) if limit > 0
   end
 
-  def self.prime? number
-    number = Integer(number)
-    return false if number < 2
-
-    trial_divisions = (2..Math.sqrt(number).to_i)
-    trial_divisions.none? { |d| number % d == 0 }
+  inline do |builder|
+    builder.c_singleton '
+    int prime(int num) {
+      int x;
+      for (x = 2; x < (num - 1) ; x++) {
+        if (num == 2) {
+          return 1;
+        }
+        if (num % x == 0) {
+          return x;
+        }
+      }
+      return 1;
+    }'
   end
-
+  
   private
 
   def self.gather limit
     return 2 if limit == 1
 
     range = 2...Float::INFINITY
-    range.lazy.select { |i| prime? i }.first(limit)
+    range.lazy.select { |i| prime i }.first(limit)
   end
 end
